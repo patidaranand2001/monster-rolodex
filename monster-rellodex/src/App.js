e17e397b6a46176df9a95866fee6c21dfaf3e464
ANAND PATIDAR <anandpatidar603@gmail.com>
2023-09-14 14:07:46 +0530

    react project

---
+++ monster-rellodex/src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+// import React from 'react'
+
+
+function App() {
+  return (
+  <>
+ 
+<Navbar title="TextUtils"/>
+  </>
+  );
+}
+
+export default App;
